cd44de15689a718e24413af5ecbf82f61e0b771b
yogakurniawan <yoga.kwn@gmail.com>
2016-07-04 17:19:25 +0700

    chore() unit test refinement

--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -1,32 +1,57 @@
-describe('controllers', () => {
-  let vm;
+describe('Controllers:: main.controllers', () => {
+  let vm, isSelected;
 
   beforeEach(angular.mock.module('stethoscope'));
 
-  beforeEach(inject(($controller, webDevTec, toastr) => {
-    spyOn(webDevTec, 'getTec').and.returnValue([{}, {}, {}, {}, {}]);
-    spyOn(toastr, 'info').and.callThrough();
-
+  beforeEach(inject(($controller) => {
     vm = $controller('MainController');
+    spyOn(vm, 'toggleOpen');
+    spyOn(vm, 'isSelected').and.returnValue(false);
+    spyOn(vm, 'selectPage');
+    spyOn(vm, 'isPageSelected');
   }));
 
-  it('should have a timestamp creation date', () => {
-    expect(vm.creationDate).toEqual(jasmine.any(Number));
+  it('tracks that toggleOpen was called', () => {
+    vm.toggleOpen('home');
+    expect(vm.toggleOpen).toHaveBeenCalledWith('home');
   });
 
-  it('should define animate class after delaying timeout', inject($timeout => {
-    $timeout.flush();
-    expect(vm.classAnimation).toEqual('rubberBand');
-  }));
-
-  it('should show a Toastr info and stop animation when invoke showToastr()', inject(toastr => {
-    vm.showToastr();
-    expect(toastr.info).toHaveBeenCalled();
-    expect(vm.classAnimation).toEqual('');
-  }));
-
-  it('should define more than 5 awesome things', () => {
-    expect(angular.isArray(vm.awesomeThings)).toBeTruthy();
-    expect(vm.awesomeThings.length === 5).toBeTruthy();
+  it('tracks that isSelected was called', () => {
+    isSelected = vm.isSelected();
+    expect(vm.isSelected).toHaveBeenCalled;
   });
+
+  it('when isSelected is called returns the requested value', () => {
+    isSelected = vm.isSelected();
+    expect(openedSection).toEqual(false);
+  });
+
+  it('it should not affect vm.openedSection value', () => {
+    vm.toggleOpen('home');
+    expect(vm.openedSection).toEqual(null);
+  });
+
+  it('tracks that toggleOpen was called', () => {
+    vm.toggleOpen('home');
+    expect(vm.toggleOpen).toHaveBeenCalledWith('home');
+  });
+  // it('should have a timestamp creation date', () => {
+  //   expect(vm.creationDate).toEqual(jasmine.any(Number));
+  // });
+
+  // it('should define animate class after delaying timeout', inject($timeout => {
+  //   $timeout.flush();
+  //   expect(vm.classAnimation).toEqual('rubberBand');
+  // }));
+
+  // it('should show a Toastr info and stop animation when invoke showToastr()', inject(toastr => {
+  //   vm.showToastr();
+  //   expect(toastr.info).toHaveBeenCalled();
+  //   expect(vm.classAnimation).toEqual('');
+  // }));
+
+  // it('should define more than 5 awesome things', () => {
+  //   expect(angular.isArray(vm.awesomeThings)).toBeTruthy();
+  //   expect(vm.awesomeThings.length === 5).toBeTruthy();
+  // });
 });
