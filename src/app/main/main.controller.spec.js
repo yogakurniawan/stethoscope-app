442224a687f332596104f1f529d1563d62d59926
yogakurniawan <yoga.kwn@gmail.com>
2016-07-05 14:43:08 +0700

    chore(unit test) add unit test for main.controller

--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -1,57 +1,41 @@
 describe('Controllers:: main.controllers', () => {
-  let vm, isSelected;
+  let vm, isSelected, isPageSelected;
 
   beforeEach(angular.mock.module('stethoscope'));
 
   beforeEach(inject(($controller) => {
     vm = $controller('MainController');
-    spyOn(vm, 'toggleOpen');
-    spyOn(vm, 'isSelected').and.returnValue(false);
-    spyOn(vm, 'selectPage');
-    spyOn(vm, 'isPageSelected');
+    spyOn(vm, 'toggleOpen').and.callThrough();
+    spyOn(vm, 'isSelected').and.callThrough();
+    spyOn(vm, 'selectPage').and.callThrough();
+    spyOn(vm, 'isPageSelected').and.callThrough();
   }));
 
-  it('tracks that toggleOpen was called', () => {
+  it('should ensure controller.openedSection has the correct value', () => {
     vm.toggleOpen('home');
+    expect(vm.openedSection).toEqual('home');
     expect(vm.toggleOpen).toHaveBeenCalledWith('home');
   });
 
-  it('tracks that isSelected was called', () => {
-    isSelected = vm.isSelected();
+  it('should ensure controller.openedSection has the correct value when triggered from controller.isSelected', () => {
+    vm.toggleOpen('main');
+    isSelected = vm.isSelected('main');
+    expect(vm.openedSection).toEqual('main');
+    expect(isSelected).toEqual(true);
     expect(vm.isSelected).toHaveBeenCalled;
   });
 
-  it('when isSelected is called returns the requested value', () => {
-    isSelected = vm.isSelected();
-    expect(openedSection).toEqual(false);
+  it('should set vm.currentSection with "subscription" and vm.currentPage with "user"', () => {
+    vm.selectPage('subscription', 'user');
+    expect(vm.currentSection).toEqual('subscription');
+    expect(vm.currentPage).toEqual('user');
+    expect(vm.selectPage).toHaveBeenCalledWith('subscription', 'user');
   });
 
-  it('it should not affect vm.openedSection value', () => {
-    vm.toggleOpen('home');
-    expect(vm.openedSection).toEqual(null);
+  it('should ensure controller.currentPage has the correct value', () => {
+    vm.selectPage('subscription', 'user');
+    isPageSelected = vm.isPageSelected('user');
+    expect(isPageSelected).toBe(true);
+    expect(vm.isPageSelected).toHaveBeenCalled;
   });
-
-  it('tracks that toggleOpen was called', () => {
-    vm.toggleOpen('home');
-    expect(vm.toggleOpen).toHaveBeenCalledWith('home');
-  });
-  // it('should have a timestamp creation date', () => {
-  //   expect(vm.creationDate).toEqual(jasmine.any(Number));
-  // });
-
-  // it('should define animate class after delaying timeout', inject($timeout => {
-  //   $timeout.flush();
-  //   expect(vm.classAnimation).toEqual('rubberBand');
-  // }));
-
-  // it('should show a Toastr info and stop animation when invoke showToastr()', inject(toastr => {
-  //   vm.showToastr();
-  //   expect(toastr.info).toHaveBeenCalled();
-  //   expect(vm.classAnimation).toEqual('');
-  // }));
-
-  // it('should define more than 5 awesome things', () => {
-  //   expect(angular.isArray(vm.awesomeThings)).toBeTruthy();
-  //   expect(vm.awesomeThings.length === 5).toBeTruthy();
-  // });
 });
