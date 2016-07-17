f902aad65938397644ffd403f3ec85a536e10641
Yoga Kurniawan <yoga.kwn@gmail.com>
2016-07-17 15:16:44 +0700

    chore(test) add unit test for menu-toggle directive

--- src/app/components/menuToggle/menu-toggle.spec.js
+++ src/app/components/menuToggle/menu-toggle.spec.js
@@ -1,35 +1,64 @@
 describe('directive menu toggle', function () {
-  let compile, scope, directiveElem, log;
+  let compile, scope;
 
   beforeEach(angular.mock.module('stethoscope'));
 
   beforeEach(() => {
-    inject(($compile, $rootScope, $log) => {
+    inject(($compile, $rootScope) => {
       compile = $compile;
       scope = $rootScope.$new();
-      log = $log;
     });
-
-    directiveElem = getCompiledElement();
   });
 
-  function getCompiledElement() {
-    var element = angular.element('<menu-toggle></menu-toggle');
-    scope.test = 'hello';
-    var compiledElement = compile(element)(scope);
-    return compiledElement;
-  }
+  it('Should call parent directive whatever it is supposed to', function () {
+    var sectionObj = {
+      name: 'Subscription',
+      type: 'toggle',
+      pages: [
+        {
+          name: 'Users',
+          type: 'link',
+          state: 'home'
+        },
+        {
+          name: 'Practices',
+          state: 'home.beers.porters',
+          type: 'link'
+        }
+      ]
+    };
+    var parentController = {
+      openedSection: null,
+      isSelected: function (section) {
+        return this.openedSection === section;
+      },
+      toggleOpen: function (section) {
+        this.openedSection = (this.openedSection === section ? null : section);
+      }
+    }
 
-  it('should have md-button element', function () {
-    log.log(directiveElem);
-    var mdButtonElement = directiveElem.find('md-button');
-    log.log(mdButtonElement);
-    expect(mdButtonElement).toBeDefined();
-  });
+    spyOn(parentController, 'isSelected').and.callThrough();
+    spyOn(parentController, 'toggleOpen').and.callThrough();
 
-  it('should have mock controller', function () {
-    var scope = directiveElem.scope();
-    expect(scope).toBeDefined();
-    expect(scope.test).toEqual('hello');
+    var element = angular.element('<div><menu-toggle><menu-toggle></div>');
+    element.data('$parentController', parentController);
+    scope.section = sectionObj;
+    scope.isSelected = function () {
+      return parentController.isSelected(scope.section);
+    };
+    scope.toggle = function () {
+      parentController.toggleOpen(scope.section);
+    };
+    compile(element)(scope);
+    var menuToggleScope = element.find('menu-toggle').scope();
+    // Act
+    menuToggleScope.toggle();
+    var isSelected = menuToggleScope.isSelected();
+
+    //Assert
+    expect(isSelected).toEqual(true);
+    expect(menuToggleScope.isSelected).not.toEqual(undefined);
+    expect(parentController.isSelected).toHaveBeenCalled();
+    expect(parentController.toggleOpen).toHaveBeenCalled();
   });
 });
