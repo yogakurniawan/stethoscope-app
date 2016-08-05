d73d973386d6ad7aa208c73e153005b741f1850c
Yoga Kurniawan <yoga.kwn@gmail.com>
2016-08-05 07:00:04 +0700

    dev(enhancement) registration page

--- src/app/account/registration/registration.controller.js
+++ src/app/account/registration/registration.controller.js
@@ -2,33 +2,52 @@ export class RegistrationController {
   constructor() {
     'ngInject';
     this.model = {
-      firstName: 'Obi Wan'
+      firstName: null,
+      lastName: null
     };
     this.fields = [
       {
-        "elementAttributes": {
-          "layout": "row",
-          "layout-sm": "column"
-        },
-        "fieldGroup": [
-          {
-            "key": "firstName",
-            "className": "flex",
-            "type": "input",
-            "templateOptions": {
-              "label": "First Name"
-            }
-          },
-          {
-            "key": "lastName",
-            "className": "flex",
-            "type": "input",
-            "templateOptions": {
-              "label": "Last Name"
-            }
-          }
-        ]
+        "key": "practiceName",
+        "type": "input",
+        "templateOptions": {
+          "label": "Practice Name"
+        }
       },
+      {
+        "type": "select",
+        "key": "title",
+        "templateOptions": {
+          "label": "Title",
+          "multiple": false,
+          "labelProp": "firstName",
+          "valueProp": "id",
+          "options": [
+            { firstName: "Mr.", id: 1 },
+            { firstName: "Mrs.", id: 2 },
+            { firstName: "Dr.", id: 3 }
+          ],
+          "onOpen": () => {
+            // console.log('select is opened');
+          },
+          "onClose": () => {
+            // console.log('select is closed');
+          }
+        }
+      },
+      {
+        "key": "firstName",
+        "type": "input",
+        "templateOptions": {
+          "label": "First Name"
+        }
+      },
+      {
+        "key": "lastName",
+        "type": "input",
+        "templateOptions": {
+          "label": "Last Name"
+        }
+      }
     ];
   }
 
